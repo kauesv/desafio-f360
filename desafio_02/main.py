63add7736c2064c93796039b3516e9ed0c7e0022
kaue_sv <kauesousavieira534@gmail.com>
2024-08-13 13:46:57 -0300

    organização do desafio 02 e instalação do pandas

--- desafio_02/main.py
+++ desafio_02/main.py
@@ -1,5 +1,4 @@
 import os
-import json
 import logging
 from datetime import datetime
 import sys
@@ -12,7 +11,7 @@ nome_programa = os.path.basename(sys.argv[0])
 
 # ----------------
 #   Diretórios
-dir_data_01 = r"B:\Meus Documentos\Documentos\projetos\desafio-f360\dados\desafio_01"
+dir_data_02 = r"B:\Meus Documentos\Documentos\projetos\desafio-f360\dados\desafio_02"
 dir_log = r"B:\Meus Documentos\Documentos\projetos\desafio-f360\dados\logs"
 
 # ----------------
@@ -25,52 +24,3 @@ file_handler = logging.FileHandler(arquivo_log, encoding='utf-8')
 file_handler.setFormatter(logging.Formatter('%(asctime)s(%(process)d)%(levelname)s %(message)s'))
 logging.basicConfig(level=logging.INFO, handlers=[file_handler])
 logging.info(f"Nome do programa: {nome_programa}")
-
-# ----------------
-#   Carrega arquivo .env
-load_dotenv()
-
-# ----------------
-#   Variaveis
-api_key_openweathermap = os.getenv('APIKEY_1')
-api_key_weatherstack = os.getenv('APIKEY_2')
-
-# ----------------
-#   Obtem coordenadas do arquivo json
-arquivo_json = dir_data_01 + "/coordenadas.json"
-logging.info(f"arquivo JSON: {arquivo_json}")
-try:
-    with open(arquivo_json, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-except:
-    logging.error("ERRO:", exc_info=True)
-    raise
-
-# ----------------
-#   Resultado openweathermap
-logging.info(f"Executando Openweathermap")
-list_result = []
-for coord in data:
-    nome = coord['nome']
-    lat = coord['lat']
-    long = coord['long']
-    logging.info(f"Coordenadas: {coord}")
-
-    try:
-        result = obtem_dados_openweathermap(nome, lat, long, api_key_openweathermap)
-        print(json.dumps(result, indent=4, ensure_ascii=False))
-
-        list_result.append(result)
-    except:
-        print('Houve um erro no openweathermap')
-
-#print(list_result)
-
-# ----------------
-# Resultado weatherstack
-logging.info(f"Executando Weatherstack")
-try:
-    result = obtem_dados_weatherstack("Sao Paulo", api_key_weatherstack)
-    print(json.dumps(result, indent=4, ensure_ascii=False))
-except:
-    print('Houve um erro no weatherstack')
